c04a60f91094b534c6598bbd1fe54040e2e7f547
evan999 <emeshberge@gmail.com>
2019-11-13 23:43:24 -0500

    Method overriding used to draw different shape types.

---
+++ Polymorphism_MethodOverriding/Canvas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle");
+                        break;
+
+                    case ShapeType.Rectangle:
+                        Console.WriteLine("Draw a rectangle");
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
